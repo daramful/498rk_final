620ca4c1b91bf6900e1bd40d2df1edea10e1b4c1
Ginny <helee295@gmail.com>
2017-12-11 06:03:38 -0600

    frontpage only

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -7,18 +7,18 @@ import styles from './Home.scss'
 
 class Home extends Component {
 
-	constructor(props){
-		super(props);
-		console.log("constructor");
-		this.state={
-			isLoggedIn: false,
-			query: "",
-			track: "",
-			artists: []
-		};
-	}
+    constructor(props){
+        super(props);
+        console.log("constructor");
+        this.state={
+            isLoggedIn: false,
+            query: "",
+            track: "",
+            artists: []
+        };
+    }
 
-	componentWillMount(){
+    componentWillMount(){
         console.log("componentWillMount");
        
     }
@@ -53,32 +53,32 @@ class Home extends Component {
     startNew(event){
 
     }
-	joinExist(event){
-    	
+    joinExist(event){
+        
     }
-	logOut(event){
-		this.setState({
-			isLoggedIn: false
-		});
-	}
+    logOut(event){
+        this.setState({
+            isLoggedIn: false
+        });
+    }
     render() {
-    	
+        
         return(
             <div className="home">
-            	<div className="ui fixed inverted menu">
-            		<div className="ui container">	
-        				<div className="menu item">
-            				MIC DROP
-    					</div>
-    					<div className="menu item">
+                <div className="ui fixed inverted menu">
+                    <div className="ui container">  
+                        <div className="menu item">
+                            <i id="mic" className="fa fa-microphone" aria-hidden="true"> </i> DROP
+                        </div>
+                        <div className="menu item">
                             Home
                         </div>
-        				<div className="menu item">
+                        <div className="menu item">
                             About
                         </div>
                         <div className="menu item right" />
                         <Link to="/login">
-        					<Button className="ui yellow button">
+                            <Button className="ui yellow button">
                                 Login
                             </Button>
                         </Link>
@@ -88,69 +88,50 @@ class Home extends Component {
                             </Button>
                         </Link>
                         <Button className="ui black button">
-            				Help
-        				</Button>	
-            	   </div>
+                            Help
+                        </Button>   
+                   </div>
                </div>
                 <div className="ui main text container">
-                	<h1 className="ui header">
-                		Welcome to MIC DROP
-            		</h1> 
-            		<p>Description Everywhere</p>
-            		<p>Description Everywhere</p>
-            		<p>Description Everywhere</p>
-            		<p>Description Everywhere</p>
-            		<p>Description Everywhere</p>
-            		<p>Description Everywhere</p>
-            		<p>Description Everywhere</p>
-            		<p>Description Everywhere</p>
-            		<p>Description Everywhere</p>
-            		<p>Description Everywhere</p>
-            		<p>Description Everywhere</p>
-            		<p>Description Everywhere</p>
-            		<p>Description Everywhere</p>
-            		<p>Description Everywhere</p>
-            		<p>Description Everywhere</p>
-            		<p>Description Everywhere</p>
-            		<p>Description Everywhere</p>
-            		<p>Description Everywhere</p>
-            		<p>Description Everywhere</p>
-            		<p>Description Everywhere</p>
-            		<p>Description Everywhere</p>
-            		<p>Description Everywhere</p>
-            		<p>Description Everywhere</p>
-            		<p>Description Everywhere</p>
-            		<p>Description Everywhere</p>
-            		<p>Description Everywhere</p>
-            		<p>Description Everywhere</p>
-            		<p>Description Everywhere</p>
-            		<p>Description Everywhere</p>
-            		<p>Description Everywhere</p>
-            		<p>Description Everywhere</p>
-            		<p>Description Everywhere</p>
-            		<p>Description Everywhere</p>
-            	</div>
-            	<div className="ui inverted vertical footer segment">
-            		<div className="ui center aligned container">
-            			<div className="ui vertical inverted small divided list">
-            				<div className="asdsad">
-            					<h2 className="ui inverted header">Developers</h2>
-            					<p>hlee295</p>
-            					<p>jsong78</p>
-            					<p>ykim164</p>
-            					<p>hpark125</p>
-        					</div>
-        				</div>
-        				<div className="ui inverted section divider"></div>
-        				<div className="ui horizontal inverted small divided list">
-        					<p>CS498 RK1 Final Project</p>
-        					<p>University of Illinois at Urbana-Champaign</p>
-        				</div>
-    				</div>
-				</div>
+
+                    <div className="mainVideo container">
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/ckdsJ-LaCvM?start=32&rel=0&autoplay=1" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>                    </div>
+                    <hr />
+                    <div className="ui header" id="animated_div">
+                            <h1>What is <i id="mic" className="fa fa-microphone" aria-hidden="true"></i> Drop?</h1>
+                    </div> 
+
+                    <div className="description">
+                        <ul>
+                            <li>Option to autoplay the list to keep the party going</li>
+                            <li>Automatic music recommendation based on user’s song selection</li>
+                            <li>Ability to add can add and delete music to and from the list within the channel</li>
+                        </ul>
+                    </div>
+
+                </div>
+                <div className="ui inverted vertical footer segment">
+                    <div className="ui center aligned container">
+                        <div className="ui vertical inverted small divided list">
+                            <div className="asdsad">
+                                <h2 className="ui inverted header">Developers</h2>
+                                <p>hlee295</p>
+                                <p>jsong78</p>
+                                <p>ykim164</p>
+                                <p>hpark125</p>
+                            </div>
+                        </div>
+                        <div className="ui inverted section divider"></div>
+                        <div className="ui horizontal inverted small divided list">
+                            <p>CS498 RK1 Final Project</p>
+                            <p>University of Illinois at Urbana-Champaign</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
 }
 
 export default Home
+
